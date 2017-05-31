027b4c7cac9e6c5cbb228e8013df42835360e64d
郑松岚 <zhengsl@hhcc.io>
2017-05-31 19:21:58 +0800

    1. 添加欢迎界面。

---
+++ app/src/main/java/com/zsl/switchlanguage/WelcomActivity.java
@@ -0,0 +1,31 @@
+package com.zsl.switchlanguage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+
+public class WelcomActivity extends AppCompatActivity {
+
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcom);
+
+        mHandler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                if (msg.what == 1) {
+                    Intent intent = new Intent(WelcomActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+                return true;
+            }
+        });
+
+        mHandler.sendEmptyMessageDelayed(1,600);
+    }
+}
